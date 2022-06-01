e841421a7c00023597712a930d7e0ab42e5ca3bf
xProsper <xprosper.play@gmail.com>
2022-06-01 14:59:11 -0400

    fix: attempt to fix CGAthin font not working after build

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,7 @@
 <svelte:head>
 	<title>Prender</title>
 	<meta name="description" content="Render to <pre> tag" />
+	<link href="src/app.css?family=CGAthin" rel="stylesheet">
 </svelte:head>
 
 <section><div>TEST <b>D</b>IV</div><pre>TEST <b>P</b>RE</pre></section>
