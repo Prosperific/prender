17bd91225d5838e0892aa62ef711a8cbe517c6eb
xProsper <xprosper.play@gmail.com>
2022-06-01 17:56:40 -0400

    fix: attempt to fix CGAthin

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,8 @@
 <svelte:head>
 	<title>Prender</title>
 	<meta name="description" content="Render to <pre> tag" />
-	<link rel="preload" href="fonts/CGA/Web437_IBM_CGAthin.woff2" as="font" type="font/woff2">
-	<link rel="preload" href="fonts/CGA/Web437_IBM_CGA.woff2" as="font" type="font/woff2">
+	<link rel="preload" href=".svelte-kit/cloudflare/fonts/CGA/Web437_IBM_CGAthin.woff2" as="font" type="font/woff2">
+	<link rel="preload" href=".svelte-kit/cloudflare/fonts/CGA/Web437_IBM_CGA.woff2" as="font" type="font/woff2">
 </svelte:head>
 
 <section><div>TEST <b>D</b>IV</div><pre>TEST <b>P</b>RE</pre></section>
